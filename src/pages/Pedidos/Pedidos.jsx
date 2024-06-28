d4b84ebffed703ce840df10017c32728cb4ca6ea
Gabriela Nagamuta <gnagamuta03@gmail.com>
2024-06-28 14:51:24 -0300

    add api list and delete order

--- src/pages/Pedidos/Pedidos.jsx
+++ src/pages/Pedidos/Pedidos.jsx
@@ -1,11 +1,62 @@
-import {Menu} from '../../components/Menu/Menu'
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import {Menu} from '../../components/Menu/Menu';
 import styles from './Pedidos.module.css';
 
 export const Pedidos = () =>  {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await api.get("/orders");
+        setOrders(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar pedidos:', error);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  async function handleDeleteOrder(id) {
+    try {
+      await api.delete("/order", {
+        params: { id }
+      });
+      const allOrders = orders.filter((order) => order.id !== id);
+      setOrders(allOrders);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className={styles.pedidos}>
       <Menu></Menu>
       <h1>Pedidos</h1>
+      {orders.length === 0 ? (
+        <p>Nenhum pedido encontrado</p>
+      ) : (
+        orders.map((order) => (
+          <div key={order.id} className="order">
+            <h3>Pedido #{order.id}</h3>
+            <p>Nome: {order.name}</p>
+            <p>Telefone: {order.phone}</p>
+            <p>Endereço: {order.address}</p>
+            <p>Forma de Pagamento: {order.paymentMethod}</p>
+            <h4>Itens:</h4>
+            <ul>
+              {order.orderItems.map((item, index) => (
+                <li key={index}>
+                  {item.product.name} - Quantidade: {item.quantity}
+                </li>
+              ))}
+            </ul>
+            <button onClick={() => handleDeleteOrder(order.id)}>Cancelar pedido</button>
+          </div>
+        ))
+      )}
     </div>
   );
 }
